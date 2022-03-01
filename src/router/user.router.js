bea457536581994ca25ccefdaedf7b5ac55560fa
Ahmed Mohamed Abo Edrees <ahmededrees30@gmail.com>
2022-03-01 07:12:13 +0200

    feat: remove createUSer from userRouter

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -3,9 +3,6 @@ const { UserController } = require("../controller/user.controller");
 
 const userRouter = express.Router();
 
-userRouter
-  .route("/users/:id?")
-  .get(UserController.getAllOrOne)
-  .post(UserController.createUser);
+userRouter.route("/users/:id?").get(UserController.getAllOrOne);
 
 module.exports = userRouter;
