d2543b86f61d1942a0822495658464b6c70c1eba
Ahmed Mohamed Abo Edrees <ahmededrees30@gmail.com>
2022-03-01 07:50:06 +0200

    fix: change /signup to /register

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const { AuthController } = require("../controller/auth.controller");
-const { validateLoginBody } = require("../middlewares/bodyValidator.mw");
+const {
+  validateLoginBody,
+  validateRegisterBody,
+} = require("../middlewares/bodyValidator.mw");
 
 const authRouter = express.Router();
 
 authRouter.post("/login", validateLoginBody, AuthController.handelLogin);
-authRouter.post("/signup", AuthController.handelRegister);
+authRouter.post(
+  "/register",
+  validateRegisterBody,
+  AuthController.handelRegister
+);
 
 module.exports = authRouter;
