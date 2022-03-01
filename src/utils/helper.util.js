90aaed4e3b45ce8bf3f996152aab9aeb5e431105
Ahmed Mohamed Abo Edrees <ahmededrees30@gmail.com>
2022-03-01 07:39:58 +0200

    fix: check is register with password

--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -1,5 +1,5 @@
 module.exports.isRegisterd = async (collection, email, password) => {
-  doc = await collection.findOne({ email: email }).exec();
+  doc = await collection.findOne({ email, password }).exec();
   if (!doc) return false;
 
   return true;
